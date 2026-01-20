01e576d6093af1a1e0f9040e9e23b95b68efff4e
Abdur Rauf <abdurraufrx@gmail.com>
2026-01-20 22:17:16 +0600

    product delete option for delete

---
+++ controller/delete_product.php
@@ -0,0 +1,23 @@
+<?php
+include 'db.php';
+session_start();
+
+
+if(!isset($_SESSION['role']) || $_SESSION['role'] != 'Admin') {
+    exit("Unauthorized Access! Only Admin can delete products.");
+}
+
+if(isset($_GET['id'])) {
+    $id = mysqli_real_escape_string($conn, $_GET['id']);
+    
+    
+    $sql = "DELETE FROM products WHERE id = '$id'";
+    
+    if(mysqli_query($conn, $sql)) {
+        header("Location: manage_inventory.php");
+        exit();
+    } else {
+        echo "Error deleting product: " . mysqli_error($conn);
+    }
+}
+?>
